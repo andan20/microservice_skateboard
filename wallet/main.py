613cf6073c963d296038b193ca98f1cb5bfd552b
andan20 <Danilov.andrey2006@yandex.ru>
2025-07-13 17:35:42 +0300

    new "ban" functions, add and delete balance

--- wallet/main.py
+++ wallet/main.py
@@ -10,17 +10,25 @@ app = FastAPI()
 def on_startup():
     wait_for_db()
 
-@app.get("/")
-async def root():
-    return {"message": "Skateboad rental service"}
 
 
+@app.get("/info/{username}")
+async def get_info(username: str, session: SessionDep):
+    username = username.lower()
+    user = session.exec(select(Wallet).where(Wallet.username == username)).first()
+    return user
 
 @app.get("/balance/{username}")
 async def check_balance(username: str, session: SessionDep):
     username = username.lower()
-    balance = session.exec(select(Wallet).where(Wallet.username == username)).all()
-    return balance
+    user = session.exec(select(Wallet).where(Wallet.username == username)).first()
+    return user.balance
+
+@app.get("/ban/{username}")
+async def is_user_ban(username: str, session: SessionDep):
+    username = username.lower()
+    user = session.exec(select(Wallet).where(Wallet.username == username)).first()
+    return user.is_ban
 
 
 @app.post("/register/{username}")
@@ -32,13 +40,49 @@ async def new_user(username: str, balance: int, session: SessionDep):
     if balance < 0 or balance > 1e9:
         return
 
-
     wallet = Wallet(username=username, balance=balance)
     session.add(wallet)
     session.commit()
     session.refresh(wallet)
     return wallet
 
+
+@app.post("/addmoney/{username}")
+async def add_money(username: str, to_add: int, session: SessionDep):
+    if to_add < 0 or to_add > 1e9:
+        return
+    username = username.lower()
+    user = session.exec(select(Wallet).where(Wallet.username == username)).first()
+    if not user:
+        return
+
+    if user.balance + to_add > 1e9:
+        return
+
+    user.balance += to_add
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@app.post("/deletemoney/{username}")
+async def add_money(username: str, to_delete: int, session: SessionDep):
+    if to_delete < 0 or to_delete > 1e9:
+        return
+    username = username.lower()
+    user = session.exec(select(Wallet).where(Wallet.username == username)).first()
+    if not user:
+        return
+
+    if user.balance - to_delete < 0:
+        user.is_ban = True
+
+    else:
+        user.balance -= to_delete
+
+    session.commit()
+    session.refresh(user)
+    return user
 
 @app.get("/health")
 async def health_check():
--- wallet/models/wallet.py
+++ wallet/models/wallet.py
@@ -5,3 +5,4 @@ class Wallet(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True, nullable=False)
     username: str = Field(index=True, unique=True)
     balance: int = Field(default=0)
+    is_ban: bool = Field(default=False)
